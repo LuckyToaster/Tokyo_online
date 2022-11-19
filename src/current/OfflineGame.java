c424f09321fb25997d7ff080ea236b123dc97e7b
Senén Marqués <91315326+LuckyToaster@users.noreply.github.com>
2022-11-19 18:58:07 +0100

    FML

--- src/current/OfflineGame.java
+++ src/current/OfflineGame.java
@@ -20,7 +20,9 @@ public class OfflineGame {
 	private List<Player> players;
 	private ListIterator<Player> pIter;
 	private Dice dice;
-	private boolean newRound;
+	private boolean newRound, believe;
+	private Player p;
+	private int deceitN;
 
 	public OfflineGame() {
 		players = new ArrayList<>();
@@ -79,63 +81,65 @@ public class OfflineGame {
 	
 	
 	private void gameLoop() {
-		Player p;
-		int deceitN = 0;
-		String ready = "\n\tIs that you, ",
-		beliefMsg = ", Do you believe ";
-		boolean believe;
+		deceitN = 0;
+		String ready = "\n\tIs that you, ", beliefMsg = ", Do you believe ";
 		clearScreen();
 
 		while (players.size() > 1) {
 			pIter = players.listIterator();
+
 			while (pIter.hasNext()) {
 				p = pIter.next();
-				
-				if (players.size() == 1) 
+
+				if (players.size() == 1)
 					break;
 
 				clearScreen();
+				handleNewRound();
 
-				if (newRound) {
-					dice.clearHistory();
-					dice.shake();
-					printStats(p, dice);
-					dice.printDrawing();
-					newRound = false;
-				} else {
-					believe = getYesOrNo("\n\t" + p.name + beliefMsg + deceitN + "? > ", in);
-					
-					if (believe && dice.get() != deceitN) // TRICKED
-						dice.setPrev(deceitN);
-					else if (!believe && deceitN != dice.get()) { // OUTWITTED
-						if (pIter.hasPrevious()) {
-							pIter.previous();
-							p = pIter.previous();
-						} else {
-							for (int i = 0; i < players.size()-1; i++)
-								pIter.next();
-							p = pIter.next();
-						}
+				believe = getYesOrNo("\n\t" + p.name + beliefMsg + deceitN + "? > ", in);
+
+				handleTricked();
+				handleWrong();
+
+				if (!believe && deceitN != dice.get()) { // OUTWITTED
+					handleDeath(p, "... and DIED\n");
+					if (pIter.previousIndex() >= 1) { // try 2 or whatever tf
+						pIter.previous();
+						p = pIter.previous();
 						looseLife(p, "\n\t" + p.name + " lost a life", WHITE);
 						handleDeath(p, "... and DIED\n");
-					} else if (!believe && dice.get() == deceitN) { // WRONG
-						looseLife(p, "\n\tYou lost a life");
-						handleDeath(p, " ... and you DIED\n");
+						handleWinner();
+						pIter.next();
+						p = pIter.next();
+					} else {
+						for (int i = 0; i < players.size() - 1; i++)
+							pIter.next();
+						p = pIter.next();
+						looseLife(p, "\n\t" + p.name + " lost a life", WHITE);
+						handleDeath(p, "... and DIED\n");
+						for (int i = players.size() - 1; i > 0; i--) // >= ??
+							pIter.previous();
+						p = pIter.previous();
 					}
+					newRound = true;
+				}
 
+				if (!handleNewRound()) {
 					dice.shake();
 					printStats(p, dice);
 					dice.printDrawing();
 				}
-				
+
 				deceitN = getInt("\tWhat will you say?: ", in);
-				
+
 				handleInvalidDeceitNumber(p, deceitN);
 				handleDeath(p, "\t YOU DIED");
 				userPrompt("\n\t⏩⏩ 🤜PASS🤜 THE 🎲DIE🎲 ⏩⏩", WHITE, 1000);
 			}
-		} out.println("\n\n\t" + players.get(0).name + " 🗿 (chad) iS THE WINNER! 🎉🎉");
-		
+		}
+		out.println("\n\n\t" + players.get(0).name + " 🗿 (chad) iS THE WINNER! 🎉🎉");
+
 	}
 
 	
@@ -172,8 +176,47 @@ public class OfflineGame {
 	}
 	
 
+
+	private boolean handleWinner() {
+		if (players.size() == 1) {
+			out.println("\n\n\t" + players.get(0).name + " 🗿 (chad) iS THE WINNER! 🎉🎉");
+			return true;
+		} else return false;
+	}
+	
+	
+	// OK
+	private boolean handleNewRound() {
+		if (newRound) {
+			dice.clearHistory();
+			dice.shake();
+			printStats(p, dice);
+			dice.printDrawing();
+			newRound = false;
+			return true;
+		} else return false;
+	}
+	
+	
+	// OK
+	private void handleTricked() {
+		if (believe && dice.get() != deceitN) {
+			userPrompt("\n\tYou were tricked, it was actually " + dice.get());
+			dice.set(deceitN);
+		}
+	}
+	
+	// OK
+	private void handleWrong() {
+		if (!believe && dice.get() == deceitN) { // WRONG
+			looseLife(p, "\n\tYou lost a life");
+			handleDeath(p, " ... and you DIED\n");
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		//OfflineGame game = new OfflineGame();
+		OfflineGame game = new OfflineGame();
 
 		//System.out.println("😂");
 		List<Integer> l = Arrays.asList(0,1,2,3,4);
@@ -181,19 +224,19 @@ public class OfflineGame {
 		int c = 0;
 
 		/*
-		while (c < 3) {
+		//while (c < 3) {
 			li = l.listIterator();
-			while (li.hasNext())
+			for (int i = 0; i < l.size(); i++)
 				System.out.println(li.next());
-			c++;
-		}
-		*/
+			//c++;
+		//}
 		
 		li = l.listIterator();
 		System.out.println(li.next());
 		System.out.println(li.next());
 		System.out.println(li.previous());
 		System.out.println(li.previous());
+		*/
 	}
 	
 }
